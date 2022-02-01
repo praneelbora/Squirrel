981aa4b7ffec805eabba6b2e664102275dcb0815
Praneel Tejpal Bora <praneelbora@gmail.com>
2022-02-02 00:20:44 +0530

    Ignore

--- squirrel.py
+++ squirrel.py
@@ -12,6 +12,7 @@ GAME = pygame.display.set_mode((900,600))
 BACK = pygame.image.load("Assets/tile.jpg")
 BACK = pygame.transform.scale(BACK,(WIDTH,HEIGHT))
 
+SCORE =0
 
 NUT = pygame.image.load("Assets/nut.png")
 NUT = pygame.transform.scale(NUT,(50,50))
@@ -19,6 +20,7 @@ NUT = pygame.transform.scale(NUT,(50,50))
 SQUIRREL=pygame.image.load("Assets/squirrel.png")
 SQUIRREL=pygame.transform.scale(SQUIRREL,(110,110))
 
+NUT_COLLECTED = pygame.USEREVENT +1
 
 def draw(squirrel,nut):
     GAME.blit(BACK,(0,0))
@@ -29,8 +31,11 @@ def draw(squirrel,nut):
     pygame.display.update()
 
 
-def move_nut(nut):
+def move_nut(squirrel,nut):
     nut.y +=VELOCITY
+    if squirrel.colliderect(nut):
+        pygame.event.post(pygame.event.Event(NUT_COLLECTED))
+
 
 def move_squirrel(keypress,squirrel):
     if (keypress[pygame.K_LEFT] and squirrel.x-VELOCITY>0):
@@ -38,20 +43,24 @@ def move_squirrel(keypress,squirrel):
     if (keypress[pygame.K_RIGHT] and squirrel.x+VELOCITY+squirrel.width<WIDTH):
         squirrel.x += VELOCITY 
 
+def main():
+    running = True
+    clock = pygame.time.Clock()
 
-running = True
-clock = pygame.time.Clock()
-
-squirrel = pygame.Rect(100,450,110,110)
-nut = pygame.Rect(100,100,50,50)
-GAME.fill(BG)
-while running:
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running=False
-    keypress = pygame.key.get_pressed()
-    move_squirrel(keypress,squirrel)
-    move_nut(nut)
-    draw(squirrel,nut)  
-pygame.quit()
+    squirrel = pygame.Rect(100,450,110,110)
+    nut = pygame.Rect(100,100,50,50)
+    GAME.fill(BG)
+    while running:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running=False
+        if event.type == NUT_COLLECTED:
+            SCORE +=1
+        keypress = pygame.key.get_pressed()
+        move_squirrel(keypress,squirrel)
+        move_nut(squirrel,nut)
+        draw(squirrel,nut)  
+    pygame.quit()
+if __name__ == '__main__':
+    main()
