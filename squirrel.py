9f5a802f81e53aead275f26b7cf37fcba7287841
Praneel Tejpal Bora <praneelbora@gmail.com>
2022-02-02 10:26:24 +0530

    Error 1

--- squirrel.py
+++ squirrel.py
@@ -1,6 +1,6 @@
 from telnetlib import GA
 import pygame
-
+import random
 WIDTH, HEIGHT = 900,600
 BG = (255,255,255)
 BLACK=(0,0,0)
@@ -34,10 +34,16 @@ def draw(squirrel,nut,SCORE):
     pygame.display.update()
 
 
-def move_nut(squirrel,nut):
-    nut.y +=VELOCITY
-    if squirrel.colliderect(nut):
-        pygame.event.post(pygame.event.Event(NUT_COLLECTED))
+def move_nut(squirrel,nut,nuts):
+    
+    for nut in nuts:
+        nut.y +=VELOCITY
+        if squirrel.colliderect(nut):
+            pygame.event.post(pygame.event.Event(NUT_COLLECTED))
+            nuts.remove(nut)
+        elif nut.y + VELOCITY> 650:
+            nuts.remove(nut)
+
 
 
 
@@ -52,11 +58,18 @@ def main():
     clock = pygame.time.Clock()
     SCORE =0
     nuts=[]
+    time = 0
     squirrel = pygame.Rect(100,450,110,110)
-    nut = pygame.Rect(100,100,50,50)
+    
     GAME.fill(BG)
     while running:
+        
+        time +=1
+        loc=  random.randrange(0,850,50)
         clock.tick(FPS)
+        if time %100 ==0:
+            nut = pygame.Rect(loc,100,50,50)
+            nuts.append(nut)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running=False
@@ -64,7 +77,7 @@ def main():
             SCORE +=1
         keypress = pygame.key.get_pressed()
         move_squirrel(keypress,squirrel)
-        move_nut(squirrel,nut)
+        move_nut(squirrel,nut,nuts)
         draw(squirrel,nut,SCORE)  
     pygame.quit()
 if __name__ == '__main__':
